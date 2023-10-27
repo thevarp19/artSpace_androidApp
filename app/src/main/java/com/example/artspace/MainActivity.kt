163e757a4192980e3b15addc692cc09e0c644aed
Yerlan Jumabayev <thevarp18@gmail.com>
2023-10-28 03:54:06 +0600

    add artworktitle function

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.data.EmptyGroup.name
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +34,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceScreen(modifier: Modifier = Modifier) {
+}
+@Composable
+fun ArtworkTitle(
+    @StringRes title: Int,
+    @StringRes year: Int
+) {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Artwork title
+        Text(
+            text = stringResource(id = title),
+            fontWeight = FontWeight.Bold,
+            color = colorResource(id = R.color.blue_100),
+            fontSize = 32.sp
+        )
+
+        // Artwork year
+        Text(
+            text = "— ${stringResource(id = year)} —",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            color = colorResource(id = R.color.gray_300)
+        )
+    }
 }
 
 
